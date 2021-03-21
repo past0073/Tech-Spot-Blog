661782146bc1b8bc94be535c40960a1f35325cf2
AP <past0073@umn.edu>
2021-03-21 10:16:54 -0500

    Added blog.js model and dashboardRoutes file.

---
+++ controllers/api/dashboardRoutes.js
@@ -0,0 +1,116 @@
+const router = require('express').Router();
+const { User, Blog } = require('../../models');
+
+//Using the '/api/dashboard' route
+
+//Get all user blog posts
+router.get('/', async (req, res) => {
+    try {
+        const blogData = await Blog.findAll({
+            where: {
+                user_id: req.session.user_id,
+            }
+        });
+        const blogs = blogData.map((blog) => blog.get({ plain: true}));
+
+        res.render('dashboard', {
+            blogs,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+//Writing a new blog post
+router.post('/new', async (req, res) => {
+    try {
+        const blogData = await Blog.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+        res.status(200).json(blogData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+//Updating a blog post by ID
+router.put('/edit/:id', async (req, res) => {
+    try {
+        const blogData = await Blog.update(req.body, {
+            where: {
+                id: req.params.id,
+            }
+        });
+        if (!blogData) {
+            res.status(404).json({ message: "No post found with that ID!" });
+            return;
+        }
+        res.status(200).json(blogData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//Deleting a blog post by ID
+router.delete('/:id', async (req, res) => {
+    try {
+        const blogData = await Blog.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            }
+        });
+        if (!blogData) {
+            res.status(404).json({ message: "No post found with that ID!" })
+        }
+        res.status(200).json(blogData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+// router.post('/login', async (req, res) => {
+//   try {
+//     const userData = await User.findOne({ where: { email: req.body.email } });
+
+//     if (!userData) {
+//       res
+//         .status(400)
+//         .json({ message: 'Incorrect email or password, please try again' });
+//       return;
+//     }
+
+//     const validPassword = await userData.checkPassword(req.body.password);
+
+//     if (!validPassword) {
+//       res
+//         .status(400)
+//         .json({ message: 'Incorrect email or password, please try again' });
+//       return;
+//     }
+
+//     req.session.save(() => {
+//       req.session.user_id = userData.id;
+//       req.session.logged_in = true;
+      
+//       res.json({ user: userData, message: 'You are now logged in!' });
+//     });
+
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
+
+// router.post('/logout', (req, res) => {
+//   if (req.session.logged_in) {
+//     req.session.destroy(() => {
+//       res.status(204).end();
+//     });
+//   } else {
+//     res.status(404).end();
+//   }
+// });
+
+module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,3 +1,12 @@
 const User = require('./User');
+const Blog = require('./Blog');
 
-module.exports = { User };
+User.hasMany(Blog, {
+    foreignKey: 'user_id',
+});
+
+Blog.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+module.exports = { User, Blog };
